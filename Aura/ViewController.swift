d22a41dc9a1c7ed7b8af4311b3a024ea5dbcc166
Maheen Khalid <maheenkhalid08@gmail.com>
2016-05-09 16:10:40 +0500

    Initial Commit

---
+++ Aura/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aura
+//
+//  Created by Maheen Khalid on 5/9/16.
+//  Copyright © 2016 Maheen Khalid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AuraTests/AuraTests.swift
@@ -0,0 +1,36 @@
+//
+//  AuraTests.swift
+//  AuraTests
+//
+//  Created by Maheen Khalid on 5/9/16.
+//  Copyright © 2016 Maheen Khalid. All rights reserved.
+//
+
+import XCTest
+@testable import Aura
+
+class AuraTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
